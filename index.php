6258a75d00e238b6c6bb49eb404ca24a03b1c5af
francescoP02 <file.partipilo@gmail.com>
2022-06-14 15:17:21 +0200

    Added product class

--- index.php
+++ index.php
@@ -6,6 +6,19 @@ Il pagamento avviene con la carta di credito, che non deve essere scaduta.
 BONUS:
 Alcuni prodotti (es. antipulci) avranno la caratteristica che saranno disponibili solo in un periodo particolare (es. da maggio ad agosto). -->
 
+<?php 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL & ~ E_NOTICE);
+
+require_once __DIR__ . "/Product.php";
+
+$food = new Product("conad", "miaomiao", 4, "crocchette per gatti");
+var_dump($food);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,6 +28,10 @@ Alcuni prodotti (es. antipulci) avranno la caratteristica che saranno disponibil
     <title>PHP OOP 2</title>
 </head>
 <body>
+
+<ul>
+    <li><?php echo $food->printInfo() ?></li>
+</ul>
     
 </body>
 </html>
